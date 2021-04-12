fc713485ffac1c05f7fa036242cddb7b1ee4e0ed
Clement <clement.gutel@gmail.com>
2021-04-12 17:33:03 +0200

    Added helper to verify webhook signature.

---
+++ QuickBooksSharp/Webhooks/WebhooksHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace QuickBooksSharp.Webhooks
+{
+    public class WebhooksHelper
+    {
+        /// <summary>
+        /// Returns whether the webhook request was signed by Intui
+        /// </summary>
+        /// <param name="intuitSignature">Value from the HTTP header "intuit-signature"</param>
+        /// <param name="webhookVerifierToken">The webhook verifier token located in the Intuit Developer dashboard</param>
+        /// <param name="requestBody">The full HTTP requests body</param>
+        /// <returns></returns>
+        public static bool IsAuthenticWebhook(string intuitSignature, string webhookVerifierToken, string requestBody)
+        {
+            var verifierTokenBytes = Encoding.UTF8.GetBytes(webhookVerifierToken);
+            var jsonBytes = Encoding.UTF8.GetBytes(requestBody);
+            var hmac = new HMACSHA256(verifierTokenBytes);
+            var hmacBytes = hmac.ComputeHash(jsonBytes);
+            var hash = Convert.ToBase64String(hmacBytes);
+            return hash == intuitSignature;
+        }
+    }
+}
